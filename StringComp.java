5502407430bb13204a5db4c8d63d3a4cb3ba2bf2
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-01-02 11:20:08 +0530

    StringComp

---
+++ StringComp.java
@@ -0,0 +1,28 @@
+public class StringComp {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+
+       
+        System.out.println(s1.equals("Hello"));
+        System.out.println(s1.equals("Hi"));
+
+
+        System.out.println(s1.equals("Hello"));
+        System.out.println(s1.equals("Hi"));
+
+        System.out.println(sai());
+    }
+
+    static boolean sai() {
+        String s1 = "Hello";
+
+       System.err.println(s1.equals("Hello"));
+
+        return (s1.equals("Hello"));
+
+
+
+
+     
+    }
+}
