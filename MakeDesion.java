a510c857d82ffa6bfbf35283caa833fe5d370df1
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-01-02 11:44:35 +0530

    makedesion

---
+++ MakeDesion.java
@@ -0,0 +1,13 @@
+public class MakeDesion {
+    public static void main(String[] args) {
+        int num = 15;
+
+        if (num > 0) {
+            System.out.println("Positive Number");
+        } else if (num < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
