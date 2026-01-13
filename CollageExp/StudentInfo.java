f9c22fd06ddc65fa9d5f5e2ad2571103d591a6c9
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-01-13 20:26:14 +0530

    studentinfo

---
+++ CollageExp/StudentInfo.java
@@ -0,0 +1,35 @@
+package CollageExp;
+import java.util.*;
+
+public class StudentInfo {
+    
+     
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Student ID: ");
+        int id = sc.nextInt();
+        
+
+
+        System.out.print("Enter Student CGPA ");
+        double CGPA= sc.nextDouble();
+
+        System.out.println("INPERMATION OF STUDENT");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);    
+        System.out.println("CGPA: " + CGPA);
+
+
+
+
+
+
+
+
+    }
+}
