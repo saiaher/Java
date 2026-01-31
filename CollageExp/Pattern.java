12ef4333d58e8349e87992ce012b237ce5610f35
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-01-31 19:44:09 +0530

    patten_using_nested_looping

--- CollageExp/Pattern.java
+++ CollageExp/Pattern.java
@@ -6,9 +6,10 @@ public class Pattern {
 
         for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print( " * ");
+                System.out.print("* ");
             }
             System.out.println();
+          
         }
     }
     
