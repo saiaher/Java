315aeaacd148480071906f4b0bf381ff87a8a44a
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-02-08 18:12:40 +0530

    Askjava

---
+++ UDEMY_WORKSHOP/AskJava.java
@@ -0,0 +1,14 @@
+package UDEMY_WORKSHOP;
+
+public class AskJava {
+    int sai = 10;
+    int jay = 20;
+    public static void main(String[] args) {
+        AskJava obj = new AskJava();
+        System.out.println("sai is bigger than jay: " + (obj.sai > obj.jay));
+
+        int x = 10;
+        int y = 20;
+        System.out.println("x is smaller than y: " + (x < y));
+    }
+}
