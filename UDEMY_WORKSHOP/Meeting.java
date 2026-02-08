0c9c925b365937bb853c5da93b3cb0ceff93e998
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-02-08 17:53:51 +0530

    meeting

---
+++ UDEMY_WORKSHOP/Meeting.java
@@ -0,0 +1,17 @@
+package UDEMY_WORKSHOP;
+
+public class Meeting {
+
+    public static void main(String[] args) {   
+
+        double x =10234.34;
+        double y = 1234.23;
+        double z = 54674.43;
+        
+        System.out.println((int)x);
+        System.out.println((int)y);
+        System.out.println((int)z);
+    
+    }
+}
+    
