453763242f94b702c3d8a09128aab7cb100c0991
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-02-08 16:39:55 +0530

    first_program

---
+++ UDEMY_WORKSHOP/FirstProgram.java
@@ -0,0 +1,14 @@
+package UDEMY_WORKSHOP;
+
+public class FirstProgram {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+        System.out.println("This is my first program");
+
+    }
+    
+}
---
+++ UDEMY_WORKSHOP/Points.java
@@ -0,0 +1,14 @@
+package UDEMY_WORKSHOP;
+
+public class Points {
+
+    public static void main(String[] args) {
+        int x = 0;
+        int finalResult = 137;
+        
+
+        System.out.println("points =" +x);
+        System.out.println("final result is " + finalResult);
+    }
+    
+}
