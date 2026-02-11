9be7f56ab3be65df8fa4837801c94309a4ff70e1
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-02-11 12:48:05 +0530

    evenor odd

---
+++ UDEMY_WORKSHOP/EvenOrOdd.java
@@ -0,0 +1,14 @@
+package UDEMY_WORKSHOP;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 19; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is even.");
+            } else {
+                System.out.println(i + " is odd.");
+            }
+        }
+    }
+    
+}
