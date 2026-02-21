0b27e62daeb15ab2300bf51cd4dbd92e016886a7
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-02-21 23:25:32 +0530

    exams

---
+++ UDEMY_WORKSHOP/Exams.java
@@ -0,0 +1,14 @@
+package UDEMY_WORKSHOP;
+
+public class Exams {
+    
+    public static void main(String[] args) {
+        
+        String seats[] = {"sai","om","rakesh","aai","sam"};
+
+        for(int i=0;i<=5;i++){
+            System.out.println(seats[0+i] + " you in index " + (i));
+        }
+    }
+    
+}
