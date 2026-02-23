64240e505f2012af4a1cda9931a2def66a79d9fb
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-02-23 17:38:22 +0530

    REceipt

---
+++ UDEMY_WORKSHOP/Arrays/Workbook_6_8/Receipt.java
@@ -0,0 +1,29 @@
+package UDEMY_WORKSHOP.Arrays.Workbook_6_8;
+
+public class Receipt {
+
+    public static void main(String[] args){
+
+        String apples[] = {"gala ", "sita fal",  "red tap"};
+
+        int price[] = {100, 120, 150};
+
+        for(int i = 0; i < price.length; i++){          // you can you both price and apple for lenght
+            System.out.println(apples[i] + " - " + price[i]);
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    
+}
+
