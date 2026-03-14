b241d425d19afb882a83341f89d1655516bf8811
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-03-14 22:15:58 +0530

    airline using constructure

---
+++ UDEMY_WORKSHOP/OOPL/WORKBOOK_7_2/Main.java
@@ -0,0 +1,16 @@
+package UDEMY_WORKSHOP.OOPL.WORKBOOK_7_2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person p1 = new Person("sai aher", "indian", "14-11-2006", 10);
+
+        System.out.println(p1.name);
+        System.out.println(p1.nationality);
+        System.out.println(p1.dateOfBirth);                 
+        System.out.println(p1.seatNumber);
+        
+    }
+
+    
+}
---
+++ UDEMY_WORKSHOP/OOPL/WORKBOOK_7_2/Person.java
@@ -0,0 +1,16 @@
+package UDEMY_WORKSHOP.OOPL.WORKBOOK_7_2;
+
+public class Person {
+    String name;
+    String nationality;
+    String dateOfBirth;
+    int seatNumber;
+
+    public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.seatNumber = seatNumber;
+    }
+
+}
