ff6882bc7b66eaf1a890a38cc29c8bcc1067a673
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-03-16 23:33:38 +0530

    tostring

---
+++ UDEMY_WORKSHOP/OOPL/WORKBOOK_7_7/Main.java
@@ -0,0 +1,22 @@
+package UDEMY_WORKSHOP.OOPL.WORKBOOK_7_7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person("sai aher","india","14-11-2006",16);
+
+          
+
+         if (p1.applyPassport()) {
+            p1.setpassport();
+
+
+
+            
+            }    System.out.println(p1);
+            
+        
+    }
+    
+    }
