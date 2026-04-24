542fdd0a88a1d4d5e34860220b9fc2caa2ea5ea6
Omkar Aher <aher.omkar.15et7021@gmail.com>
2026-04-24 18:33:58 +0530

    magazine

---
+++ UDEMY_WORKSHOP/OOPL/PRACTICE_3/PART_1/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Magazine magazine1 = new Magazine("Magazine 1", "Publisher 1", 1, 2020);
+        System.out.println(magazine1.getTitle());
+
+        
+    }
+}
